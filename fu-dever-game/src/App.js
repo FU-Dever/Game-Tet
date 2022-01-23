d9af6547e3dd344dc141ec03d1e16473851b920d
Nhantv0302 <nhantv0302@gmail.com>
2022-01-23 22:17:27 +0700

    Add Sound

--- fu-dever-game/src/App.js
+++ fu-dever-game/src/App.js
@@ -4,79 +4,93 @@ import Button from "./components/button";
 import CountDown from "./components/count_down";
 import Options from "./components/options";
 import TopPlayer from "./components/top_player";
+import Tet from "./Audio/Tet.mp3";
+import { Howl, Howler } from "howler";
+const Audio = [{ sound: Tet, label: "" }];
+export default class App extends React.Component {
+  SoundPlay = (src) => {
+    const sound = new Howl({
+      src,
+    });
+    sound.play();
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      optionIndex: [],
+      isCountDown: false,
+      popUpOpen: false,
+    };
+  }
 
-export default class App extends React.Component{
+  btnAction(btn) {
+    if (btn === "start") {
+      let optionIndex1 = Math.floor(Math.random() * 5);
+      let optionIndex2 = Math.floor(Math.random() * 5);
+      let optionIndex3 = Math.floor(Math.random() * 5);
+      let optionIndex = [optionIndex1, optionIndex2, optionIndex3];
+      alert(optionIndex);
 
-
-    constructor(props){
-        super(props);
-        this.state = {
-            optionIndex: [],
-            isCountDown:false,
-            popUpOpen: false,
-        }
+      this.setState({
+        optionIndex: optionIndex,
+        popUpOpen: true,
+      });
+    } else {
+      this.setState({
+        isCountDown: true,
+      });
     }
+  }
 
-    btnAction(btn){
-        if(btn==="start"){
-            let optionIndex1 = Math.floor(Math.random() * 5);  
-            let optionIndex2 = Math.floor(Math.random() * 5);  
-            let optionIndex3 = Math.floor(Math.random() * 5);  
+  stopCountDown() {
+    this.setState({
+      isCountDown: false,
+    });
+  }
 
-            let optionIndex = [optionIndex1, optionIndex2, optionIndex3]
-            alert(optionIndex)
+  popUpClose() {
+    this.setState({
+      popUpOpen: false,
+    });
+  }
+  RenderButtonAndSound = () => {
+    return Audio.map((soundObj, index) => {
+      return (
+        <button key={index} onClick={() => this.SoundPlay(soundObj.sound)}>
+          {soundObj.label}
+        </button>
+      );
+    });
+  };
+  render() {
+    Howler.volume(1.0);
+    return (
+      <div className=".App">
+        <div id="title-app">FU-DEVER BẦU CUA</div>
+        <div id="wrap-content">
+          {this.RenderButtonAndSound()}
+          <TopPlayer />
+          <Options />
+          {this.state.isCountDown && (
+            <CountDown stopCountDown={() => this.stopCountDown()} />
+          )}
+        </div>
 
-            this.setState({
-                optionIndex: optionIndex,
-                popUpOpen: true
-            })
-        }
-        else{
-            this.setState({
-                isCountDown: true
-            })
-        }
-    }
+        <div id="wrap-btns">
+          <Button
+            name="BẮT ĐẦU CHỌN"
+            clicked={() => this.btnAction("countDown")}
+          />
+          <Button name="QUAY" clicked={() => this.btnAction("start")} />
+        </div>
 
-    stopCountDown(){
-        this.setState({
-            isCountDown: false
-        })
-    }
-
-
-    popUpClose(){
-        this.setState({
-            popUpOpen: false
-        })
-    }
-
-
-    render(){
-        return(
-
-            <div className=".App">
-                <div id="title-app">FU-DEVER BẦU CUA</div>
-                <div id="wrap-content">
-                    <TopPlayer/>
-                    <Options/>
-                    {
-                        this.state.isCountDown &&
-                        <CountDown stopCountDown={()=>this.stopCountDown()}/>
-                    }
-                </div>
-                    
-                <div id="wrap-btns">
-                    <Button name="BẮT ĐẦU CHỌN" clicked={()=>this.btnAction("countDown")}/>
-                    <Button name="QUAY" clicked={()=>this.btnAction("start")}/>
-                </div>
-
-                {
-                    this.state.popUpOpen &&
-                    <BingoPopUp close={()=>this.popUpClose()} data={this.state.optionIndex}/>
-                }
-            </div>
-        );
-    }
+        {this.state.popUpOpen && (
+          <BingoPopUp
+            close={() => this.popUpClose()}
+            data={this.state.optionIndex}
+          />
+        )}
+      </div>
+    );
+  }
 }
-
---
+++ fu-dever-game/src/components/Sound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Sound extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
