bd30b2dff9b8184dfc5d26b1408c56dcd7908f23
vbachdoan <vbachdoan@gmail.com>
2022-01-23 20:25:32 +0700

    done giao diện

--- fu-dever-game/src/App.js
+++ fu-dever-game/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import BingoPopUp from "./components/bingo_pop_up";
 import Button from "./components/button";
-import CountDown from "./components/countdown";
+import CountDown from "./components/count_down";
 import Options from "./components/options";
 import TopPlayer from "./components/top_player";
 
@@ -10,16 +11,24 @@ export default class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            optionIndex: 1,
+            optionIndex: [],
+            isCountDown:false,
+            popUpOpen: false,
         }
     }
 
     btnAction(btn){
         if(btn==="start"){
-            let optionIndex = Math.floor(Math.random() * 6) + 1;
-            console.log(optionIndex)
+            let optionIndex1 = Math.floor(Math.random() * 5);  
+            let optionIndex2 = Math.floor(Math.random() * 5);  
+            let optionIndex3 = Math.floor(Math.random() * 5);  
+
+            let optionIndex = [optionIndex1, optionIndex2, optionIndex3]
+            alert(optionIndex)
+
             this.setState({
                 optionIndex: optionIndex,
+                popUpOpen: true
             })
         }
         else{
@@ -36,24 +45,38 @@ export default class App extends React.Component{
     }
 
 
+    popUpClose(){
+        this.setState({
+            popUpOpen: false
+        })
+    }
+
+
     render(){
         return(
+
             <div className=".App">
                 <div id="title-app">FU-DEVER BẦU CUA</div>
                 <div id="wrap-content">
                     <TopPlayer/>
                     <Options/>
                     {
-                        this.isCountDown &&
-                        <CountDown endCountDown={()=>this.stopCountDown()}/>
+                        this.state.isCountDown &&
+                        <CountDown stopCountDown={()=>this.stopCountDown()}/>
                     }
                 </div>
+                    
                 <div id="wrap-btns">
                     <Button name="BẮT ĐẦU CHỌN" clicked={()=>this.btnAction("countDown")}/>
                     <Button name="QUAY" clicked={()=>this.btnAction("start")}/>
                 </div>
-                <h1>Vi tri duoc chon demo data: {this.state.optionIndex}</h1>
+
+                {
+                    this.state.popUpOpen &&
+                    <BingoPopUp close={()=>this.popUpClose()} data={this.state.optionIndex}/>
+                }
             </div>
         );
     }
 }
+
