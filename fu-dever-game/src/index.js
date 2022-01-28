6eb791d67e5bb25d139549dffcfb2d3cb2192d5c
Nguyễn Thị Thùy Dung <70100724+dngthy@users.noreply.github.com>
2022-01-28 21:38:47 +0700

    Update index.js

--- fu-dever-game/src/index.js
+++ fu-dever-game/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import UserInterface from './UserInterface';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/host" element={<App/>}/>
+      <Route path="/" element={<UserInterface/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
